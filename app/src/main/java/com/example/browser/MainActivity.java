3eed8d7456ded5c5da02d05812e32db1d786b361
tikka <tikkanen.kuura@gmail.com>
2021-03-25 17:45:43 +0200

    added html file reading

--- app/src/main/java/com/example/browser/MainActivity.java
+++ app/src/main/java/com/example/browser/MainActivity.java
@@ -15,7 +15,7 @@ import android.widget.ImageButton;
 public class MainActivity extends AppCompatActivity {
 
     EditText url;
-    Button siirrybtn;
+    Button siirrybtn, shoutoutbtn, backbtn;
     ImageButton reload;
     WebView web;
     Context context;
@@ -30,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
 
         url = (EditText) findViewById(R.id.editURL);
         siirrybtn = (Button) findViewById(R.id.button);
+        shoutoutbtn = (Button) findViewById(R.id.button2);
+        backbtn = (Button) findViewById(R.id.button3);
         reload = (ImageButton) findViewById(R.id.imageButton);
         web = (WebView) findViewById(R.id.web);
 
@@ -40,24 +42,42 @@ public class MainActivity extends AppCompatActivity {
         web.setWebViewClient(new WebViewClient());
 
         //permission for javascript
-        web.getSettings().getJavaScriptEnabled();
+        web.getSettings().setJavaScriptEnabled(true);
 
         //web.loadUrl("https://skinfo.dy.fi");
 
         siirrybtn.setOnClickListener(v -> {
-            //siirry sivustolle
+            //siirry sivustolle tai renderöi tiedosto
             currentSite = "https://" + url.getText().toString();
+            System.out.println(currentSite);
 
-            if (currentSite == null || currentSite == "https://") {
+
+            if (currentSite == null || currentSite.equals("https://")) {
                 System.out.println("No address given");
+
+            } else if (currentSite.equals("https://index.html")) {
+                System.out.println("Loading file");
+                web.loadUrl("file:///android_asset/index.html");
+
             } else {
+                System.out.println("Loading site");
                 web.loadUrl(currentSite);
             }
         });
 
+        shoutoutbtn.setOnClickListener(v-> {
+            //run shoutOut() from index.html
+            web.evaluateJavascript("javascript:shoutOut()",null);
+        });
+
+        backbtn.setOnClickListener(v-> {
+            //run initialize() from index.html
+            web.evaluateJavascript("javascript:initialize()",null);
+        });
+
         reload.setOnClickListener(v -> {
             //päivitä tämänhetkinen sivu
-            if (currentSite != null && currentSite == "https://") {
+            if (currentSite != null && !currentSite.equals("https://") && !currentSite.equals("https://index.html")) {
                 web.loadUrl(currentSite);
             }
         });
